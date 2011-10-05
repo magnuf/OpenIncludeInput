79a1c0f2dd942758eaec762c3ee6224d74fc27d2
Magnus Fjell <magnus.fjell@gmail.com>
2011-10-05 12:50:41 +0200

    Added files

---
+++ open_include_input.py
@@ -0,0 +1,27 @@
+import sublime, sublime_plugin
+import os.path
+import re
+
+class OpenIncludeInput(sublime_plugin.TextCommand):
+	def run(self, view):
+
+
+		for region in self.view.sel():
+			line = str(self.view.substr(self.view.line(region)))
+			groups = re.match(r"\\(?:input|include){([^}]*)}", line)
+			if not groups:
+				sublime.status_message("Use this on a line starting with an \\include or \\input")
+			else:
+				self.openFile(groups(0))
+
+	def openFile(self, filename):
+	
+			filepath = os.path.dirname(self.view.file_name().strip()) + '/' + filename + ".tex"
+
+			if os.path.exists(filepath):
+				self.view.window().open_file(filepath)
+				sublime.status_message("Opening file " + filepath)
+			else :
+				sublime.status_message("Could not find " + filepath)
+
+		view.sel().clear()
